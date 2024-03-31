a814bd6d7a84ddc23851282762b894b8108af3f1
Jordan Chappell <jordanchappell@cordel.ai>
2024-04-01 09:18:58 +1100

    feat(runner): implement 'down' workflow

--- src/CSharp.Mongo.Migration/Core/MigrationRunner.cs
+++ src/CSharp.Mongo.Migration/Core/MigrationRunner.cs
@@ -31,15 +31,40 @@ public class MigrationRunner : IMigrationRunner {
 
     public async Task<MigrationResult> RunAsync() {
         if (_migrationLocator is null)
-            throw new ArgumentNullException(
-                nameof(_migrationLocator),
-                "Unable to run migrations without registering an `IMigrationLocator`"
-            );
+            throw MigrationLocatorException();
 
         IEnumerable<IMigration> migrations = _migrationLocator
             .GetMigrations(_migrationCollection)
             .Where(m => !m.Skip);
         return await RunMigrationsAsync(migrations);
+    }
+
+    public async Task<MigrationResult> RestoreAsync(string version) {
+        if (_migrationLocator is null)
+            throw MigrationLocatorException();
+
+        MigrationDocument document = await _migrationCollection
+            .Find(m => m.Version == version)
+            .FirstOrDefaultAsync();
+
+        if (document is null)
+            throw new ArgumentException(
+                "Migration with provided version has not been recorded in the database",
+                nameof(version)
+            );
+
+        IMigration migration = _migrationLocator
+            .GetMigrations(_migrationCollection)
+            .FirstOrDefault(m => m.Version == version);
+
+        if (migration is null)
+            throw new ArgumentException("Unable to locate migration with provided version", nameof(version));
+
+        await migration.DownAsync(_database);
+
+        return new() {
+            Steps = new List<MigrationDocument>() { document },
+        };
     }
 
     private async Task<MigrationDocument> RunMigrationAsync(IMigration migration) {
@@ -63,4 +88,9 @@ public class MigrationRunner : IMigrationRunner {
             Steps = steps,
         };
     }
+
+    private ArgumentNullException MigrationLocatorException() => new(
+        nameof(_migrationLocator),
+        "Unable to run migrations without registering an `IMigrationLocator`"
+    );
 }
--- src/CSharp.Mongo.Migration/Interfaces/IMigrationRunner.cs
+++ src/CSharp.Mongo.Migration/Interfaces/IMigrationRunner.cs
@@ -5,4 +5,5 @@ namespace CSharp.Mongo.Migration.Interfaces;
 public interface IMigrationRunner {
     public IMigrationRunner RegisterLocator(IMigrationLocator locator);
     public Task<MigrationResult> RunAsync();
+    public Task<MigrationResult> RestoreAsync(string version);
 }
