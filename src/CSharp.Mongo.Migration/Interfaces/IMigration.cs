ed2defd1df093ce6e6a574c26daffec21c371c1c
Jordan Chappell <jordan.chappell33@gmail.com>
2024-04-05 19:47:53 +1100

    docs(migration): add xml summaries to IMigration interface

--- src/CSharp.Mongo.Migration/Interfaces/IMigration.cs
+++ src/CSharp.Mongo.Migration/Interfaces/IMigration.cs
@@ -4,15 +4,42 @@ using MongoDB.Driver;
 
 namespace CSharp.Mongo.Migration.Interfaces;
 
+/// <summary>
+/// Migration script contract, provides the structure that the `MigrationRunner` requires to run and manage migrations.
+/// </summary>
 public interface IMigration {
+    /// <summary>
+    /// Human readable migration name / description.
+    /// </summary>
     public string Name { get; }
+    /// <summary>
+    /// Unique version.
+    /// </summary>
     public string Version { get; }
+    /// <summary>
+    /// Flags that this migration can be skipped, implement this member with conditional logic for more powerful
+    /// migration selection.
+    /// </summary>
     public bool Skip { get { return false; } }
 
+    /// <summary>
+    /// Function run when a migration is applied.
+    /// </summary>
+    /// <param name="database">MongoDB driver `IMongoDatabase` instance.</param>
+    /// <returns>Task representing asynchronous database operations.</returns>
     public Task UpAsync(IMongoDatabase database);
+    /// <summary>
+    /// Function run when a migration is reverted.
+    /// </summary>
+    /// <param name="database">MongoDB driver `IMongoDatabase` instance.</param>
+    /// <returns>Task representing asynchronous database operations.</returns>
     public Task DownAsync(IMongoDatabase database);
 
-    public MigrationDocument ToDocument() => new() {
+    /// <summary>
+    /// Interface method to simplify mapping an `MigrationDocument` instance.
+    /// </summary>
+    /// <returns></returns>
+    internal MigrationDocument ToDocument() => new() {
         Name = Name,
         Version = Version,
         AppliedUtc = DateTime.UtcNow,
