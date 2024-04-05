7ea65b3fd04c27c7fe1187ff92c54536a410d9af
Jordan Chappell <jordan.chappell33@gmail.com>
2024-04-05 19:54:53 +1100

    docs(database-factory): add xml summaries to DatabaseConnectionFactory

--- src/CSharp.Mongo.Migration/Infrastructure/DatabaseConnectionFactory.cs
+++ src/CSharp.Mongo.Migration/Infrastructure/DatabaseConnectionFactory.cs
@@ -2,9 +2,17 @@
 
 namespace CSharp.Mongo.Migration.Infrastructure;
 
-public static class DatabaseConnectionFactory {
+/// <summary>
+/// Manage connections to MongoDB.
+/// </summary>
+internal static class DatabaseConnectionFactory {
     private static readonly Dictionary<string, IMongoDatabase> _databases = new();
 
+    /// <summary>
+    /// Retrieve an instance of the MongoDB driver `IMongoDatabase` class with the provided connection.
+    /// </summary>
+    /// <param name="connectionString">MongoDB connection uri string.</param>
+    /// <returns>New or, if already connected, existing database instance with provided uri.</returns>
     public static IMongoDatabase GetDatabase(string connectionString) {
         if (_databases.ContainsKey(connectionString))
             return _databases[connectionString];
