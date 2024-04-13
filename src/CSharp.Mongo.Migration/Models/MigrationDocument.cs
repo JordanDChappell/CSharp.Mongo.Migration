7521288dd11a24b7bf964acab2819a35f2144cf8
Jordan Chappell <jordan.chappell33@gmail.com>
2024-04-13 17:11:46 +1000

    docs(summary): add xml summary comments to migration document properties

--- src/CSharp.Mongo.Migration/Models/MigrationDocument.cs
+++ src/CSharp.Mongo.Migration/Models/MigrationDocument.cs
@@ -7,9 +7,21 @@ namespace CSharp.Mongo.Migration.Models;
 /// Database document used to keep track of which migrations have run.
 /// </summary>
 public class MigrationDocument {
+    /// <summary>
+    /// MongoDB `ObjectId` GUID.
+    /// </summary>
     public ObjectId Id { get; set; }
+    /// <summary>
+    /// Human readable migration name.
+    /// </summary>
     public string? Name { get; set; }
+    /// <summary>
+    /// Unique migration version string, this could be a simple SemVer number, or a date stamp, or an issue number.
+    /// </summary>
     public string? Version { get; set; }
+    /// <summary>
+    /// When the migration was applied to the database.
+    /// </summary>
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime? AppliedUtc { get; set; }
 }
